bb8b3cd82e3cb89be7d8bd822fbc507893850e17
frontendprogramista <frontendprogramista@gmail.com>
2023-03-02 12:16:53 +0100

    arrow left added

--- slider/example.js
+++ slider/example.js
@@ -1,5 +1,18 @@
-let myFirstFunction = () => {
-console.log('Hey my name is Robert');
-};
-myFirstFunction();
+let bool = 5>2
+console.log(bool);
 
+let bool2 = 1===1;
+console.log(bool2);
+
+let bool3 = 1 !== 2;
+console.log(bool3);
+
+let bool4 = 'robert' === 'irena';
+console.log(bool4);
+
+let isBiggerThan10 = (number) => {
+if(number > 10) {
+    console.log('number is bigger than 10');
+}
+};
+isBiggerThan10(12);
